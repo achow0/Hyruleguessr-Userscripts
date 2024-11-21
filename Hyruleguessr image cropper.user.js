104ce3372ed25c9768e0e2379adf1ced4958937a
achow0 <66978816+achow0@users.noreply.github.com>
2024-11-20 22:38:38 -0800

    Update Hyruleguessr image cropper.user.js
    remove onurlchange code

--- Hyruleguessr image cropper.user.js
+++ Hyruleguessr image cropper.user.js
@@ -4,13 +4,12 @@
 // @description  Crops the images during each round for an extra challenge! (follow instructions within the userscript for some customization)
 // @author       achow
 // @match        https://hyruleguessr.com/*
-// @grant        window.onurlchange
 // ==/UserScript==
 
 ( function() {
     "use strict";
     const direction = "RU"; // choose D (Down), U (Up), R (Right), or L (Left) to choose which part of the image to keep
-                            // choose L or R, then U or D to choose to keep a quarter of the image
+    // choose L or R, then U or D to choose to keep a quarter of the image
     const polygonPicker = {
         "D": "0 50%, 0 100%, 100% 100%, 100% 50%",
         "U": "0 0, 100% 0, 100% 50%, 0 50%",
@@ -24,12 +23,12 @@
     const URL = "https://hyruleguessr.com/game";
     function picTest() {
         const pic = document.querySelector(".viewer-move");
-            if (!!pic && !pic.style.clipPath) {
-                pic.style.clipPath = `polygon(${polygonPicker[direction]})`;
-            }
+        if (!!pic && !pic.style.clipPath) {
+            pic.style.clipPath = `polygon(${polygonPicker[direction]})`;
+        }
     };
-    function run(info) {
-        if (!info.url.startsWith(URL)) return;
+    function run(){
+        if (!window.location.href.startsWith(URL)) return;
         picTest()
         const targetNode = document.body;
         const config = { attributes: true, childList: true, subtree: true };
@@ -39,6 +38,5 @@
         const observer = new MutationObserver(callback);
         observer.observe(targetNode, config);
     };
-    if (window.onurlchange === null) window.addEventListener("urlchange", run);
-    run({url: window.location.href});
+    run()
 })();
